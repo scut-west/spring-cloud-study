0b76636039feec9411d5078bdf524c0ceb7391f5
west <781967140@qq.com>
2019-01-10 21:37:22 +0800

    1. use localhost instead of peer1, peer2 in order to register eureka
    2. add customize eureka metadata, and test to get it

--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/controller/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/controller/MovieController.java
@@ -2,10 +2,14 @@ package com.itmuch.cloud.microservicesimpleconsumermovie.controller;
 
 import com.itmuch.cloud.microservicesimpleconsumermovie.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
@@ -13,8 +17,16 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient client;
+
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
         return this.restTemplate.getForObject("http://localhost:8000/user/" + id, User.class);
+    }
+
+    @GetMapping("/user-instance")
+    public List<ServiceInstance> showInfo() {
+        return this.client.getInstances("microservice-provider-user");
     }
 }
