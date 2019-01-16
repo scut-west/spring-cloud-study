611cd027608bad4c47a45fd82095263ff382541a
west <781967140@qq.com>
2019-01-16 22:43:30 +0800

    add Ribbon for consumer-movie project
    use Ribbon with Eureka

--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
@@ -2,12 +2,14 @@ package com.itmuch.cloud.microservicesimpleconsumermovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MicroserviceSimpleConsumerMovieApplication {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/controller/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/controller/MovieController.java
@@ -1,9 +1,12 @@
 package com.itmuch.cloud.microservicesimpleconsumermovie.controller;
 
 import com.itmuch.cloud.microservicesimpleconsumermovie.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,7 @@ import java.util.List;
 
 @RestController
 public class MovieController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
 
     @Autowired
     private RestTemplate restTemplate;
@@ -20,13 +24,22 @@ public class MovieController {
     @Autowired
     private DiscoveryClient client;
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
-        return this.restTemplate.getForObject("http://localhost:8000/user/" + id, User.class);
+        return this.restTemplate.getForObject("http://MICROSERVICE-PROVIDER-USER/user/" + id, User.class);
     }
 
     @GetMapping("/user-instance")
     public List<ServiceInstance> showInfo() {
         return this.client.getInstances("microservice-provider-user");
+    }
+
+    @GetMapping("/log-user-instance")
+    public void logUserInstance() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("MICROSERVICE-PROVIDER-USER");
+        MovieController.LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
     }
 }
