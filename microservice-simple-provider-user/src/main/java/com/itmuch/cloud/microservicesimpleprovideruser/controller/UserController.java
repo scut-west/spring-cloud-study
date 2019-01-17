2a1330e8f5c739b30f16bdb133d5d8857552110b
west <781967140@qq.com>
2019-01-17 23:22:35 +0800

    add security into microservice-provide-user and use WebSecurityConfigurerAdapter to config security

--- microservice-simple-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/controller/UserController.java
+++ microservice-simple-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/controller/UserController.java
@@ -2,11 +2,19 @@ package com.itmuch.cloud.microservicesimpleprovideruser.controller;
 
 import com.itmuch.cloud.microservicesimpleprovideruser.dao.UserRepository;
 import com.itmuch.cloud.microservicesimpleprovideruser.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
 
 @RestController
 @RequestMapping("/user")
@@ -14,9 +22,23 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/{id}")
     public User findById(@PathVariable long id) {
+        printUserInfo();
         User user = this.userRepository.getOne(id);
         return user;
+    }
+
+    private void printUserInfo() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(principal instanceof UserDetails) {
+            UserDetails user = (UserDetails) principal;
+            Collection<? extends GrantedAuthority> collection = user.getAuthorities();
+            for(GrantedAuthority c: collection) {
+                UserController.LOGGER.info("user is {}, role is {}", user.getUsername(), c.getAuthority());
+            }
+        }
     }
 }
